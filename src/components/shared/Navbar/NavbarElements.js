643238f383ea752d108dc69193c36c41883b5286
Jorge Herrera <jmherrera@indracompany.com>
2020-11-05 02:25:22 -0500

    last commit of the day

--- src/components/shared/Navbar/NavbarElements.js
+++ src/components/shared/Navbar/NavbarElements.js
@@ -7,22 +7,17 @@ export const Nav = styled.nav`
     // margin-top: -80px;
     display: flex;
     justify-content: center;
-    align-items: center;
-    font-size: 1rem;
-    position: sticky;
-    top: 0;
     z-index: 10;
     position: fixed;
     left: 0;
     top: 0;
     width: 100%;
-    z-index: 999;
+    height: auto;
+
     background-color: ${({navActive}) => (navActive? '#0bef9a8f': '#0bef9b00')};
     box-shadow: ${({navActive}) => (navActive? '0 5px 10px 0 #0bef9a8f': '#0bef9b00')};
     -webkit-box-shadow: ${({navActive}) => (navActive? '#0bef9a8f':'#0bef9b00')};
-    transition: background-color 150ms ease-in,box-shadow 150ms ease-in,-webkit-box-shadow 150ms ease-in;
-    flex-direction: row;
-    
+    transition: background-color 150ms ease-in,box-shadow 150ms ease-in,-webkit-box-shadow 150ms ease-in;   
 
     @media screen and (max-width: 960px) {
         transition: 0.8s all ease;
@@ -31,11 +26,7 @@ export const Nav = styled.nav`
 
 export const NavbarContainer = styled.div`
     display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    height: 100%;
-    z-index: 1;
-    width: 100%;
+    flex-direction: row;            
     /* padding: 0 24px; */
     max-width: calc(25%/2);
     text-transform: capitalize;
@@ -110,7 +101,7 @@ export const NavRightMenu = styled.div`
     justify-content: space-between;
     position: absolute;        
     right: 12%;
-    top: 25%;
+    top: 23%;
     margin-right: -100px;
     @media screen and (max-width: 768px) {
         display: none;
@@ -209,174 +200,6 @@ export const NavBtnClassic = styled.div`
         transition: all 0.2s ease-in-out;
         background: var(--c-active);
         color: var(--c-white);
-    }
-`
-
-// New Header
-
-export const NavUpdated = styled.header`
-    display: block;
-    position: fixed;
-    left: 0;
-    top: 0;
-    width: 100%;
-    z-index: 999;
-`;
-
-export const ContainerUpdated = styled.div`
-    width: 100% !important;
-    max-width: 1200px !important;
-    padding-right: 15px;
-    padding-left: 15px;
-    margin-right: auto;
-    margin-left: auto;
-
-    @media (min-width: 1200px) {
-        max-width: 1140px;
-    }
-    @media (min-width: 992px)  {
-        max-width: 960px;
-    }
-    @media (min-width: 768px) {
-        max-width: 720px;
-    }
-    @media (min-width: 576px)  {
-        max-width: 540px;
-    }
-    @media (min-width: 1200px) {
-        max-width: 1140px;
-    }
-    @media (min-width: 992px) {
-        max-width: 960px;
-    }
-    @media (min-width: 768px)  {
-        max-width: 720px;
-    }
-    @media (min-width: 576px)  {
-        max-width: 540px;
-    }
-
-`
-
-export const NavbarContainerUpdated = styled.div`
-    hyphens: manual;
-    box-sizing: border-box;
-    position: relative;
-    display: flex;
-    position: relative;
-    width: 100%;
-    flex-wrap: wrap;
-    align-content: flex-start;
-    transition: background 0.3s, border 0.3s, border-radius 0.3s, box-shadow 0.3s, -webkit-border-radius 0.3s, -webkit-box-shadow 0.3s;
-    max-width: 100%;
-    margin: auto;
-    color: var(--c-dark);
-    padding: 0 15px;
-    min-height: 50px;
-    align-items: center;
-    
-
-    &:before {    
-    box-sizing: border-box;
-    }
-`
-
-export const NavLeftElements = styled.div`
-    top: auto;
-    height: auto;
-    padding-bottom: 0px;
-
-    @media screen and (min-width: 1200px) {
-        display: block !important;
-        -webkit-box-flex: 1;
-        flex-grow: 1;
-        -webkit-box-align: center;
-        align-items: center;
-        flex-basis: calc((100% - 200px) / 2);
-    }
-`;
-
-export const NavLeftElementsInner = styled.nav`
-        display: block;        
-`
-
-export const NavLeftElementsLinks = styled(LinkScroll)`
-    color:#FEE7E9;
-    display:flex;
-    align-items: center;
-    text-decoration: none;
-    padding: 0 1rem;
-    height:100%;
-    cursor:pointer;
-    margin: 0 5px;
-    -webkit-box-flex: 1;
-    flex-grow: 1;
-    text-align: center;
-
-    &:hover{
-        color:#ff4e5b;
-        transition: all 0.2s ease-in-out;
-    }
-
-    &.active {
-        color:#CE94FF;
-
-    }
-`
-
-export const NavMiddleElement = styled.div`
-    align-items: center;
-    padding: 10px 0;
-    display: flex;
-    -webkit-box-align: center;
-`
-
-export const NavMiddleElementInnerImg = styled.img`
-width: auto;
-height: auto;
-`
-
-export const NavRightElements = styled.div`
-
-    top: auto;
-    height: auto;
-    padding-bottom: 0px;
-
-    @media screen and (min-width: 1200px) {
-        display: block !important;
-        -webkit-box-flex: 1;
-        flex-grow: 1;
-        -webkit-box-align: center;
-        align-items: center;
-        flex-basis: calc((100% - 200px) / 2);
-    }
-`
-
-export const NavRightElementsInner = styled.nav`
-        display: block;        
-`
-
-export const NavRightElementsLinks = styled(LinkScroll)`
-    color:#FEE7E9;
-    display:flex;
-    align-items: center;
-    text-decoration: none;
-    padding: 0 1rem;
-    height:100%;
-    cursor:pointer;
-    margin: 0 5px;
-    -webkit-box-flex: 1;
-    flex-grow: 1;
-    text-align: center;
-
-    &:hover{
-        color:#ff4e5b;
-        transition: all 0.2s ease-in-out;
-    }
-
-    &.active {
-        color:#CE94FF;
-
     }
 `
 
